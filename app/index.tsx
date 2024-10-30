5a27a8e172789547f60662c3a017ffdd09d78d3a
Praew2003 <stu6512732115@sskru.ac.th>
2024-10-30 22:26:22 +0700

    up

--- app/index.tsx
+++ app/index.tsx
@@ -3,10 +3,12 @@ import { View, Text} from 'react-native';
 
 export default function App() {
 return (
-<View style={{backgroundColor:'#ff0000'}}>
-    <View style={{backgroundColor:'#99ff00'}}><Text style={{fontSize:90}}>praew</Text></View>
-    <View style={{backgroundColor:'#fff000'}}><Text style={{fontSize:90}}>praewpan</Text></View>
-    <View style={{backgroundColor:'#f00000'}}><Text style={{fontSize:90}}>sukkasem</Text></View>
+<View style={{backgroundColor:'#9933FF'}}>
+    <View style={{backgroundColor:'#9933FF'}}><Text style={{fontSize:20}}>ชื่อ-สกุล : นางสาวแพรวพรรณ สุขเกษม (แพรว)</Text></View>
+    <View style={{backgroundColor:'#9933FF'}}><Text style={{fontSize:20}}>ที่อยู่ปัจจุบัน : 64 ม.13 ต.ด่าน อ.ราษีไศล จ.ศรีสะเกษ 33160</Text></View>
+    <View style={{backgroundColor:'#9933FF'}}><Text style={{fontSize:20}}>หมายเลขโทรศัพท์ : 0902358532</Text></View>
+    <View style={{backgroundColor:'#9933FF'}}><Text style={{fontSize:20}}>อีเมลล์ : stu6512732115@sskru.ac.th</Text></View>
 </View>
 );
 }
+
