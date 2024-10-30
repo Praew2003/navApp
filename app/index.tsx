fe6f34685cda4ce9af85dd846ab293ca48fe65c0
Praew2003 <stu6512732115@sskru.ac.th>
2024-10-30 11:32:55 +0700

    up

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+export default function App() {
+return (
+<View style={{backgroundColor:'#ff0000'}}>
+    <View style={{backgroundColor:'#99ff00'}}><Text style={{fontSize:90}}>praew</Text></View>
+    <View style={{backgroundColor:'#fff000'}}><Text style={{fontSize:90}}>praewpan</Text></View>
+    <View style={{backgroundColor:'#f00000'}}><Text style={{fontSize:90}}>sukkasem</Text></View>
+</View>
+);
+}
